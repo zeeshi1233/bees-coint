6bacecf8492114b1d0d54bb42663ec1dc7c486d4
zeeshi1233 <zk305139@gmail.com>
2025-04-17 10:25:18 -0700

    update

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ import UserRouter from "./router/UserRoute.js";
 import Transactionrouter from "./router/TransactionRoute.js";
 import investmentRouter from "./router/InvestmentRoute.js";
 import InvesmentPackagerouter from "./router/InvestmentRoutePackage.js";
-
+import cors from "cors"; // ⬅️ Add this line
 const app = express();
 const port = process.env.PORT || 3000;
-
+app.use(cors()); // ⬅️ Allow all origins
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
